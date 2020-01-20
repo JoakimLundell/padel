56bd4fae9e0749147de1a580e713fa8351fe36ed
Joakim Lundell <joakim.lundell@evry.com>
2020-01-20 14:59:24 +0100

    Create phot component

--- resources/js/components/PadelApp.vue
+++ resources/js/components/PadelApp.vue
@@ -1,110 +1,49 @@
 <template>
     <div class="container">
 
-        <padel-menu></padel-menu>
+        <padel-header>
             
-
-            <div class="section">
+            <padel-menu></padel-menu>
+        
+        </padel-header>
+            
+        <div class="section">
                 
-                <div class="columns">
+            <div class="columns">
                 
-                
-                    <div class="column">
+                <div class="column">
                        
-                            <router-view></router-view>
+                    <router-view></router-view>
                         
-                    </div>
-
-                    <div class="column">
-                    
-
-                        <div class="columns is-multiline">
-                            
-                           
-                            <div class="column is-one-third">
-                                <figure class="image is-1by1">
-                                    <img src="/img/padel3.jpg">
-                                </figure>
-                            </div>
-
-                            <div class="column is-one-third">
-                                <figure class="image is-1by1">
-                                    <img src="/img/costa-blanca.jpg">
-                                </figure>
-                            </div>
-                           
-                            <div class="column is-one-third">
-                                <figure class="image is-1by1">
-                                    <img src="/img/padeltennis.jpg">
-                                </figure>
-                            </div>
-                            
-
-                            <div class="column is-one-third">
-                                <figure class="image is-1by1">
-                                    <img src="/img/la-mata-beach.jpg">
-                                </figure>
-                            </div>
-
-
-                            <div class="column is-one-third">
-                                <figure class="image is-1by1">
-                                    <img src="/img/flag_spain.png">
-                                </figure>
-                            </div>
-
-                            <div class="column is-one-third">
-                                <figure class="image is-1by1">
-                                    <img src="/img/padel.jpg">
-                                </figure>
-                            </div>
-
-                           
-
-                            <div class="column is-one-third">
-                                <figure class="image is-1by1">
-                                    <img src="/img/map.png">
-                                </figure>
-                            </div>
-
-                           
-
-                            <div class="column is-one-third">
-                                <figure class="image is-1by1">
-                                    <img src="/img/padel2.jpeg">
-                                </figure>
-                            </div>
-
-                            <div class="column is-one-third">
-                                <figure class="image is-1by1">
-                                    <img src="/img/walk.jpg">
-                                </figure>
-                            </div>
-                            
-                            
-                          
-                        </div>
-                            
-                       
-                    </div>
-
                 </div>
 
-                <cookie-consent></cookie-consent>
-                
+                <div class="column">
+                    
+                    <padel-images></padel-images>
+                                   
+                </div>
+
             </div>
+
+            <cookie-consent></cookie-consent>
+                
+        </div>
 
     </div>
 </template>
 
 <script>
+import PadelHeader from './PadelHeader';
 import PadelMenu from './PadelMenu.vue';
 import CookieConsent from './CookieConsent.vue';
+import PadelImages from './PadelImages.vue';
     
 export default {
     components: {
+        PadelHeader,
         PadelMenu,
-        CookieConsent
+        CookieConsent,
+        PadelImages
     },
     
 }
---
+++ resources/js/components/PadelImages.vue
@@ -0,0 +1,65 @@
+<template>
+   
+    <div class="columns is-multiline">
+        
+        <div class="column is-one-third">
+            <figure class="image is-1by1">
+                <img src="/img/padel3.jpg">
+            </figure>
+        </div>
+
+        <div class="column is-one-third">
+            <figure class="image is-1by1">
+                <img src="/img/costa-blanca.jpg">
+            </figure>
+        </div>
+        
+        <div class="column is-one-third">
+            <figure class="image is-1by1">
+                <img src="/img/padeltennis.jpg">
+            </figure>
+        </div>
+        
+        <div class="column is-one-third">
+            <figure class="image is-1by1">
+                <img src="/img/la-mata-beach.jpg">
+            </figure>
+        </div>
+
+        <div class="column is-one-third">
+            <figure class="image is-1by1">
+                <img src="/img/flag_spain.png">
+            </figure>
+        </div>
+
+        <div class="column is-one-third">
+            <figure class="image is-1by1">
+                <img src="/img/padel.jpg">
+            </figure>
+        </div>
+
+        <div class="column is-one-third">
+            <figure class="image is-1by1">
+                <img src="/img/map.png">
+            </figure>
+        </div>
+
+        <div class="column is-one-third">
+            <figure class="image is-1by1">
+                <img src="/img/padel2.jpeg">
+            </figure>
+        </div>
+
+        <div class="column is-one-third">
+            <figure class="image is-1by1">
+                <img src="/img/walk.jpg">
+            </figure>
+        </div>
+        
+    </div> 
+
+</template>
+
+<script>
+export default {}
+</script>
