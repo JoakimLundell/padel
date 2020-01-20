ad100df7b8b8616917c9426a778657a256056083
Joakim Lundell <joakim.lundell@evry.com>
2020-01-20 14:59:47 +0100

    Add header component

---
+++ resources/js/components/PadelHeader.vue
@@ -0,0 +1,28 @@
+<template>
+    <header>
+        <div class="container has-text-centered logo-area">
+            <!--img src="/img/logo.png" width="183px" height="24px"-->
+        </div>
+        <slot></slot>
+    </header>
+    
+</template>
+
+<script>
+
+export default {
+
+    data:  {
+    
+    
+    },
+
+    
+}
+</script>
+
+<style scoped>
+    .logo-area {
+        padding: 10px 0 1px;
+    }
+</style>
