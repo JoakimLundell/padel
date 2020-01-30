0463e7336658b318e34f3369cb0c418ba79b9260
Joakim Lundell <joakim.lundell@evry.com>
2020-01-30 19:02:04 +0100

    Add versoning

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,5 +14,6 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
 .sass('resources/sass/app.scss', 'public/css')
-.copy('node_modules/bulma/css/bulma.css', 'public/css/bulma.css');
+.copy('node_modules/bulma/css/bulma.css', 'public/css/bulma.css')
+.version();
 
