eef553dd15543d530811cd97351040e437f5d87e
Joakim Lundell <joakim.lundell@evry.com>
2020-02-09 13:00:00 +0100

    Add new photos

--- resources/js/components/PadelImages.vue
+++ resources/js/components/PadelImages.vue
@@ -4,25 +4,31 @@
         
         <div class="column is-one-third">
             <figure class="image is-1by1">
-                <img src="/img/padel3.jpg">
+                <!--img src="/img/padel3.jpg"-->
+                <img src="/img/image4.jpg">
+                
             </figure>
         </div>
 
         <div class="column is-one-third">
             <figure class="image is-1by1">
-                <img src="/img/costa-blanca.jpg">
+                <!--img src="/img/costa-blanca.jpg"-->
+                <img src="/img/image0.jpg">
             </figure>
         </div>
         
         <div class="column is-one-third">
             <figure class="image is-1by1">
-                <img src="/img/padeltennis.jpg">
+                <!--img src="/img/padeltennis.jpg"-->
+                <img src="/img/image1.jpg">  
             </figure>
         </div>
         
         <div class="column is-one-third">
             <figure class="image is-1by1">
-                <img src="/img/la-mata-beach.jpg">
+                <!--img src="/img/la-mata-beach.jpg"-->
+                <img src="/img/padel3.jpg">
+                  
             </figure>
         </div>
 
@@ -40,13 +46,16 @@
 
         <div class="column is-one-third">
             <figure class="image is-1by1">
-                <img src="/img/map.png">
+                <!--img src="/img/map.png"-->
+                <img src="/img/image2.jpg">  
             </figure>
         </div>
 
         <div class="column is-one-third">
             <figure class="image is-1by1">
                 <img src="/img/padel2.jpeg">
+                
+                <img src="/img/image3.jpg">
             </figure>
         </div>
 
