3519173ada67142a01d472eade182069bc9784c5
Joakim Lundell <joakim.lundell@evry.com>
2020-02-11 22:14:47 +0100

    Add auth to menu

--- resources/js/components/PadelMenu.vue
+++ resources/js/components/PadelMenu.vue
@@ -6,28 +6,56 @@
                 <router-link to="/">Info</router-link></li>
             <li v-bind:class="{ 'is-active': this.$route.path == '/sign-up' }">
                 <router-link to="/sign-up" class="is-active">Anmäl dig</router-link></li>
-            <li v-bind:class="{ 'is-active': this.$route.path == '/registrations' }">
+            
+            
+            <template v-if="authenticated">
+                <li v-bind:class="{ 'is-active': this.$route.path == '/registrations' }">
                 <router-link to="/registrations">Alla anmälda</router-link></li>
+                <li><a href="" @click.prevent="logout">Logout</a></li>
+            </template>
+            <template v-else>
+                <li v-bind:class="{ 'is-active': this.$route.path == '/login' }">
+                <router-link to="/login">Login</router-link></li>
+                
+            </template>
         </ul>
-        
+
     </div>
     
 </template>
 
 <script>
+    import {mapGetters, mapActions} from 'vuex'
 
-export default {
+    export default {
 
-    data: function() {
-        return {
-            currentRoute: this.$router.currentRoute
+        data: function() {
+            return {
+                currentRoute: this.$router.currentRoute
+            }
+        },
+
+        computed: {
+            ...mapGetters({
+                authenticated: 'auth/authenticated',
+                user: 'auth/user'
+            })
+        },
+
+        methods: {
+            ...mapActions({
+                logoutAction: 'auth/signout'
+            }),
+
+            logout() {
+                this.logoutAction().then(()=> {
+                    this.$router.replace({
+                        'name': 'home'
+                    })
+                })
+            }
         }
-    },
-
-    watch: {
-   
-  }
-}
+    }
 </script>
 
 <!--style>
